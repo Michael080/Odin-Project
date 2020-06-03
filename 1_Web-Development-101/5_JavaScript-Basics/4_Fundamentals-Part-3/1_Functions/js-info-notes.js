d393406a2313a9af2c0be648768f7b0f8f7b6609
Michael080 <mrollin808@gmail.com>
2020-06-02 19:09:20 -0600

    Example of self-describing code using functions that break the code into smaller components

--- 1_Web-Development-101/5_JavaScript-Basics/4_Fundamentals-Part-3/1_Functions/js-info-notes.js
+++ 1_Web-Development-101/5_JavaScript-Basics/4_Fundamentals-Part-3/1_Functions/js-info-notes.js
@@ -20,3 +20,45 @@ function anotherFunction() {
 }
 
 showMessages('Wambo')
+
+
+
+
+// ===========================================================================
+// Functions ==  Comments
+// Functions should be short and do exactly one thing. If that thing is big, maybe it’s worth it to split the function into a few smaller functions. Sometimes following this rule may not be that easy, but it’s definitely a good thing.
+//
+// A separate function is not only easier to test and debug – its very existence is a great comment!
+//
+// For instance, compare the two functions showPrimes(n) below. Each one outputs prime numbers up to n.
+//
+// The first variant uses a label:
+
+function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) continue nextPrime;
+        }
+
+        alert( i ); // a prime
+    }
+}
+
+// The second variant uses an additional function isPrime(n) to test for primality:
+
+function showPrimes(n) {
+
+    for (let i = 2; i < n; i++) {
+        if (!isPrime(i)) continue;
+
+        alert(i);  // a prime
+    }
+}
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if ( n % i == 0) return false;
+    }
+    return true;
+}
