0983834bc804e2af9ab9eb76d0e0dd386432d38a
Michael080 <mrollin808@gmail.com>
2021-08-08 13:47:17 -0600

    Write a constructor for making 'Book' objects.

---
+++ 2_Full-Stack-Javascript/1_Javascript/2_Organizing-your-JS/2_Objects-and-Object-Constructors/obj-constructors-ex.js
@@ -0,0 +1,16 @@
+/* Write a constructor for making “Book” objects. We will revisit this in the project at the end of this lesson. Your book objects should have the book’s title, author, the number of pages, and whether or not you have read the book */
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => {
+    const readMessage = () => this.read ? 'has read' : 'not read yet';
+    return `${title}, ${author}, ${pages}, ${title}, ${readMessage()}`;
+  }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 500, false);
+
+console.log(theHobbit.info())
