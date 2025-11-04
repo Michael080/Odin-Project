7fb24e4e01cc47a568edcee42aad96b7dc57f4cb
Michael080 <mrollin23@protonmail.me>
2025-11-03 17:52:45 -0700

    Create lesson examples
    Unit "JS Basics: Variables & Operators"

---
+++ 1_Web-Development-101/5_JavaScript-Basics/1_Variables-and-Operators/examples/script.js
@@ -0,0 +1,19 @@
+// assign vars
+let name = "john";
+let surname = "doe";
+
+console.log(name,surname);
+
+// re-assign var/s
+let age = 11;
+console.log("age:",age);
+
+age = 69; // re-assigned
+
+console.log("updated age:",`${age}`);
+
+// const (not re-assignable)
+const pi = 3.14;
+
+// old-school assignment via `var`
+var thang = "malang"; // less recommended syntax
